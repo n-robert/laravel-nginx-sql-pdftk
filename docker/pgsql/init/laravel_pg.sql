e4661cc49f21e374ec9a083e47ccabd45cc1743a
Robert N <nrobert@mail.ru>
2021-06-06 18:45:48 +0300

    Initial upload

---
+++ docker/pgsql/init/laravel_pg.sql
@@ -0,0 +1,19 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+-- DROP ROLE IF EXISTS awesome_user;
+-- CREATE ROLE awesome_user WITH LOGIN SUPERUSER PASSWORD 'awesome_password';
+
+-- Do your needed PostgresSQL stuff here
